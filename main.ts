834af9d2bc1ca7c158639382bfc020bacd494fe3
Michael Braun <57547002+michaelbraunrentonschools@users.noreply.github.com>
2020-02-27 13:25:14 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+// Write out a list of your five favorite foods here:
+
+// 1. carbanara
+
+let food1 = "carbanara"
+
+// 2. steak
+
+let food2 = "steak"
+
+// 3. ceasar salad
+
+let food3 = "ceasar salad"
+
+
+// 4. chicken
+
+let food4 = "chicken"
+
+// 5. sushi
+
+let food5 = "sushi"
+
+// Display all five favorite foods in a single array
+
+
+let listFoods = ["carbanara", "steak"]
+
+
+
+
+
